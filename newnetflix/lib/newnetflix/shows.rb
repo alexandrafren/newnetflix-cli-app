0ada18339e44aa8c8063d6d09538b994339c8d0f
Alexandra Fren <alexandra.fren@gmail.com>
2018-07-07 03:24:14 +0000

    added month data to filter scraped data

--- newnetflix/lib/newnetflix/shows.rb
+++ newnetflix/lib/newnetflix/shows.rb
@@ -2,6 +2,10 @@ class Newnetflix::Show
  attr_accessor :name, :release_year, :show_description, :runtime, :language, :genre, :cast
 
    def self.today
+     #scrapes data from whats-on-netflix.com and returns new shows added each day
+     doc = Nokogiri::HTML(open("https://www.whats-on-netflix.com/whats-new/"))
+     show_html = doc.css(".content")
+     shows = []
      show_1 = self.new
      show_1.name = "Gilmore Girls"
      show_1.release_year = "2000"
@@ -10,6 +14,7 @@ class Newnetflix::Show
      show_1.language = "English"
      show_1.genre = "Dramedy"
      show_1.cast = "Kelly Bishop, Lauren Graham, Alexis Bledel"
+     shows << show_1
 
      show_2 = self.new
      show_2.name = "TWD"
@@ -19,8 +24,39 @@ class Newnetflix::Show
      show_2.language = "English"
      show_2.genre = "Horror"
      show_2.cast = "Andrew Lincoln, Norman Reedus"
+     shows << show_2
 
-     [show_1, show_2]
+     shows
    end
+
+   def self.date
+     month = ""
+     case time.month
+     when 1
+       month = "January"
+     when 2
+       month = "February"
+     when 3
+       month = "March"
+     when 4
+       month = "April"
+     when 5
+       month = "May"
+     when 6
+       month = "June"
+     when 7
+       month = "July"
+     when 8
+       month = "August"
+     when 9
+       month = "September"
+     when 10
+       month = "October"
+     when 11
+       month = "November"
+     when 12
+       month = "December"
+     end
+
 
 end
