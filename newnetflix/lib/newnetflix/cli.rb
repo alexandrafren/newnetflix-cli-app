6e248f1aa1609a10822345b49a277e1422a3237f
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-07-06 22:35:45 -0400

    add in shows functionality

--- newnetflix/lib/newnetflix/cli.rb
+++ newnetflix/lib/newnetflix/cli.rb
@@ -7,7 +7,10 @@ class Newnetflix::CLI
   end
 
   def list_new_shows
-    puts "1.Gilmore Girls - TV14 2.The Walking Dead TV-MA"
+    shows = Newnetflix::Show.today
+    shows.each_with_index do |show, i|
+      puts "#{i+1}. #{show.name} - #{show.genre}"
+    end
   end
 
   def menu
@@ -15,15 +18,11 @@ class Newnetflix::CLI
     while input != "exit"
       puts "Which show would you like to learn more about? Enter the number of the show, or type exit"
       input = gets.chomp
-    case input
-      #will need to be variable depending on the number of new shows in a day....
-    when "1"
-      puts "Gilmore Girls is a show...."
-    when "2"
-      puts "TWD is a show about...."
-    when "exit"
-      goodbye
-    else
+      if input.to_i > 0
+        puts "#{input.to_i}. #{Newnetflix::Show.today[input.to_i - 1].name}"
+      elsif input == "exit"
+        goodbye
+      else
       puts "I do not recognize your input."
     end
   end
