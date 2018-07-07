62ff4556bbb25465bf8f414bfd83080bef2f440c
Alexandra Fren <35177685+alexandrafren@users.noreply.github.com>
2018-07-06 22:36:53 -0400

    change name of class

--- newnetflix/lib/newnetflix/shows.rb
+++ newnetflix/lib/newnetflix/shows.rb
@@ -1,11 +1,11 @@
-class Show
- attr_accessor :name, :release_year, :description, :runtime, :language, :genre, :cast
+class Newnetflix::Show
+ attr_accessor :name, :release_year, :show_description, :runtime, :language, :genre, :cast
 
    def self.today
      show_1 = self.new
      show_1.name = "Gilmore Girls"
      show_1.release_year = "2000"
-     show_1.description = "Gilmore Girls is a show about a mother and daughter, and a mother and daughter. And coffee."
+     show_1.show_description = "Gilmore Girls is a show about a mother and daughter, and a mother and daughter. And coffee."
      show_1.runtime = "42-48 minutes per episode"
      show_1.language = "English"
      show_1.genre = "Dramedy"
@@ -14,7 +14,7 @@ class Show
      show_2 = self.new
      show_2.name = "TWD"
      show_2.release_year = "2009"
-     show_2.description = "Braaaaaains."
+     show_2.show_description = "Braaaaaains."
      show_2.runtime = "45-50 minutes"
      show_2.language = "English"
      show_2.genre = "Horror"
